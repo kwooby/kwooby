d819489d9988b89eeb9e21324ab9054d29ab2ed2
Kwooby <alexcasteladavis@hotmail.com>
2026-07-30 16:25:11 -0700

    Delete photo not confirming debug

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@ const button = document.getElementById("show-password")
 const confirmPasswordButton = document.getElementById("show-confirm-password")
 
 
-const confirmDelete = document.querySelectorAll(".delete-form");
+const confirmDelete = document.querySelector(".delete-form");
 const confirmDeletePhoto = document.querySelectorAll(".delete-photo-form");
 const confirmDeleteAccount = document.querySelector(".delete-account");
 
